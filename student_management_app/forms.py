92dd3f0b84ff441318c419a3b8a80dc865ac6d36
Rahul Polasa <rpolasa@osidigital.com>
2023-09-26 20:47:15 +0530

    feat:added forms.py file to studentmanagementapp folder

---
+++ student_management_app/forms.py
@@ -0,0 +1,60 @@
+from django import forms
+from .models import User
+# from matplotlib import widgets
+
+from django.core.exceptions import ValidationError
+
+from django.forms import TextInput,EmailInput,NumberInput,Select,Textarea
+
+
+class UserForm(forms.ModelForm):
+    # phoneNumber = forms.IntegerField(validators=[validate_mobile_number])
+    password = forms.CharField()
+
+    gender = forms.ChoiceField(widget=forms.RadioSelect,
+                           choices=(     
+                                ("M", "Male"),
+                                ("F", "Female"),
+                                ("O","Other")))
+    Qualification = forms.ChoiceField(choices=(
+    ("SSC","SSC"),
+    ("Inter","Inter"),
+    ("Degree","Degree"),
+    ("PG","PG")
+    ))
+    class Meta:
+        model=User
+        fields="__all__"
+        widgets = {
+            'name': TextInput(attrs={
+                'class': "form-control",
+                'style': 'width: 280px;height: 18px;padding: 7px;border-radius: 10px;border-width: 1px;border-color: rgba(0, 0, 0, 0.2);',
+                'placeholder': 'Name'
+                }),
+            'email': EmailInput(attrs={
+                'class': "form-control", 
+                'style': 'width: 280px;height: 18px;padding: 7px;border-radius: 10px;border-width: 1px;border-color: rgba(0, 0, 0, 0.2);',
+                'placeholder': 'Email'
+                }),
+            'phoneNumber' : NumberInput(attrs={
+                'class': "form-control", 
+                'style': 'width: 280px;height: 18px;padding: 7px;border-radius: 10px;border-width: 1px;border-color: rgba(0, 0, 0, 0.2);',
+                'placeholder': 'Phone Number'
+                }),
+            'Qualification' : Select(attrs={
+                'class': "form-control", 
+                'style': 'width: 280px;height: 18px;padding: 7px;border-radius: 10px;border-width: 1px;border-color: rgba(0, 0, 0, 0.2);',
+                }),
+            'Address' : Textarea(attrs={
+                'rows' : 4,
+                'cols' : 50,
+                'style': 'width: 280px;padding: 7px;border-radius: 10px;border-width: 1px;border-color: rgba(0, 0, 0, 0.2);',
+                'placeholder': 'Address'
+                }),    
+            'username' : TextInput(attrs={
+                'class': "form-control", 
+                'style': 'width: 280px;height: 18px;padding: 7px;border-radius: 10px;border-width: 1px;border-color: rgba(0, 0, 0, 0.2);',
+                'placeholder': 'User Name'
+                }), 
+            
+                }
