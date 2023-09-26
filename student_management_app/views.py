95e0c5ce821a1e410c2ad145326c4f745a5f5969
Rahul Polasa <rpolasa@osidigital.com>
2023-09-26 20:55:17 +0530

    feat:added UserDeails function to views.py file

--- student_management_app/views.py
+++ student_management_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import User
 # Create your views here.
+
+def UserDetails(request,id):
+    user = User.objects.get(id=id)
+    context = {
+        'user' : user
+    }
+    return render(request,"UserPage.html",context)
+
